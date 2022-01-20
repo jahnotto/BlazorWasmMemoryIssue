fce61f62098ffe9dd5874063deee7ad564890691
Jahn Otto Andersen <jahn-otto.andersen@soprasteria.com>
2022-01-20 13:02:17 +0100

    Adding sample

---
+++ BlazorMem/MemTest.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorMem;
+
+public class MemTest
+{
+    private byte[]? _memBuffer = null;
+
+    public void AllocateMemory()
+    {
+        _memBuffer = new byte[512 * 1024 * 1024];
+    }
+
+    public void FreeMemory()
+    {
+        _memBuffer = null;
+        //GC.Collect(); // This is not recommended, and it has no effect in this case
+    }
+}
---
+++ BlazorMem/wwwroot/js/mem.js
@@ -0,0 +1,15 @@
+﻿
+let array = [];
+
+export function allocateArray() {
+    array = new Uint8Array(1024 * 1024 * 1024);
+    array.fill(128);
+}
+
+export function freeArray() {
+    array = [];
+}
+
+export function getMemUsage() {
+    return performance.memory.usedJSHeapSize;
+}
